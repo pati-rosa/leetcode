f85c8a5c9643702025e0072d6e7032436d204ecf
pati-rosa <patirosa995@gmail.com>
2024-10-23 11:55:43 -0300

    feat: add exercises

--- recursive/logestCommonSubsequence.js
+++ recursive/logestCommonSubsequence.js
@@ -4,23 +4,32 @@
  * @return {number}
  */
 
- var longestCommonSubsequence = function(text1, text2) {
-
+var longestCommonSubsequence = function(text1, text2) {
+    let memo = Array(text1.length).fill(null).map(() => Array(text2.length).fill(-1))
     var longestCommonRecursive = function(i,j) {
          if (i === text1.length || j === text2.length) {
             return 0;
+        } 
+
+        if(memo[i][j] !== -1)  {
+            return memo[i][j]
         }
-        
         if(text1[i] === text2[j]) {
-            return 1 + longestCommonRecursive(i+1,j+1)
+            memo[i][j] = 1 + longestCommonRecursive(i+1,j+1)
         } else {
-            return Math.max(longestCommonRecursive(i+1,j),longestCommonRecursive(i,j+1))
+             memo[i][j] = Math.max(longestCommonRecursive(i+1,j),longestCommonRecursive(i,j+1))
         }
-        
+        return memo[i][j]
     }
 
     return longestCommonRecursive(0,0)
 
 };
 
-longestCommonSubsequence('abcpsko','oiuhap')
+//In this question if we don't use memoization the execution time will be easily execed
+//Time Complexity using mesmo: O(m*n), m is the length of text1 and  n the length of the text2
+//Space Complexity: O(m*n) using a memo array of legth m*n, O(min(m,n)) is used for the recursive stack,
+//O(m*n) is the dominante
+
+// * If we dont use memoization the time complexity will be O(2ˆ(n+m)) because it create a binary tree of 
+//recursive calls
--- sorting/mergeSortedArray.js
+++ sorting/mergeSortedArray.js
@@ -22,8 +22,8 @@ var merge = function(nums1, m, nums2, n) {
 };
 
 
-//Complexidade de tempo: O(n) do primeiro laço for + O(nlog(n)) do sort do javascript = O(n + n log(n)) 
-//Complexidade de espaco: O(1)
+//Time Complexity: O(n) for + O(nlog(n)) sort = O(n + n log(n)) 
+//Space Complexity: O(1)
 
 var merge = function(nums1, m, nums2, n) {
     for (let i = m, j = 0; j < n; i++, j++) {
