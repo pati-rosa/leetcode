d3ac35cba8ad7a14857cf825a74e332fe6d76306
pati-rosa <patirosa995@gmail.com>
2024-11-22 12:14:38 -0300

    feat: balanced binary tree

--- binarySearchTree/balancedBinaryTree.js
+++ binarySearchTree/balancedBinaryTree.js
@@ -47,5 +47,128 @@ var isBalanced = function(root) {
         return isBalanced(root.left) && isBalanced(root.right)
     }
         
-    return false //nao balanceada
+    return false 
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+*/
+
+
+var isBalanced = function(root) {
+    
+    var getHeight = function(node) {
+
+        if(!node) { // 1
+            return 0
+        }
+        let height_left = getHeight(node.left)  
+        let height_right = getHeight(node.right)
+
+        if(height_right === -1 ) {
+            return -1
+        }
+
+        if(height_left === -1 ) {
+            return -1
+        }
+
+        if(Math.abs(height_left - height_right) > 1) {
+            return -1
+        }
+
+        return Math.max(height_left,height_right) + 1
+    }
+
+    if(getHeight(root) === -1) {
+        return false
+    }
+    return true
+};
+
+//[1,2,2,3,3,null,null,4,4]
+//node=1
+//left(2)
+//right(2)
+
+//node=2
+//left(3)
+//right(3)
+
+//node(3)
+//left(4)
+//right(4)
+
+//node=4 = 1
+//left(null) = 0
+//right(null) = 0
+//Math.max(0,0) + 1 = 1
+//Math.abs(0-0) = 0 < 1
+
+//voltando no 3
+//node 3 = 2
+//left(4) = 1
+//right(4) = 
+//Math.max(1,1) + 1 = 2
+
+//votando 2 = 3
+//left(3) = 2
+//right(3)  = 1
+//Math.max(2,1) + 1 =  2+1 = 3
+
+//node=3 = 1
+//left(null) = 0
+//right(null) = 0
+//Math.max(0,0) + 1 = 1
+
+//voltando node = 1
+//left(2) = 3
+//right(2) = 1
+//Math.abs(3-1) = 2 > 1 return -1
+
+//node =2 = 1
+//left(null) = 0
+//right(null) = 0
+//Math.max(0,0) + 1 = 1
+
+
+//node = 1 
+//left = getHeight(2)
+//right = getHeight(2)
+
+//node = 2
+//left = 3
+//right =null
+
+//node = 3
+//left = 4
+//right = null
+
+//node = 4
+//left = null = 0
+//right = null = 0
+//node 4 = Math.max(0,0) + 1
+//Math.abs(0-0) = 0
+
+//voltando para node 3 
+//left = 4  //1
+//right = null //0
+//node3=Math.max(1,0) + 1 = 2
+//Math.abs(1 - 0) = 1
+
+//voltando para node 2
+//left = 3
+//right = null
+//Math.abs(2 - 0) = 2 > 1 return -1
+//Math.max(2,0) +1 = 3
+
+
